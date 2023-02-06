6a23557ed11ab373bb163ee8f9885b718c9acd63
sadthudatink <124554701+sadthudatink@users.noreply.github.com>
2023-02-06 13:46:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bạn à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Darling à, e có thể cho a cơ hội làm bồ e đk',
+    desc: 'A đã thích e từ cái nhìn đầu tiên ',
+    btnYes: 'Ờm cho cơ hội',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Lỡ click rồi cho a hỏi a có cơ hội tán e k?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://instagram.com/dmqtrieu' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
